43f06800c74f7142e7f0e90d465e9e805344dd65
Stefan <stefanhe99@hotmail.com>
2018-05-15 15:10:25 +1000

    Jtextfield opens Virtualkeyboard on click

--- src/main/java/therobotpeople/urcap/GuiTextField.java
+++ src/main/java/therobotpeople/urcap/GuiTextField.java
@@ -2,42 +2,49 @@ package therobotpeople.urcap;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JTextField;
 
-public class GuiTextField extends JTextField implements ActionListener{
+public class GuiTextField extends JTextField implements MouseListener {
 
-	GuiTextField() {
-		addActionListener(this);
+
+	GuiTextField(){
+		addMouseListener(this);
 	}
 	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void mouseClicked(MouseEvent e) {
 		QWERTYKeyboard temp = new QWERTYKeyboard();
 		temp.text = this;
 		Thread t = new Thread(temp);
 		t.start();
-		
 	}
-}
-
-/*
-package_width_text.addActionListener(new ActionListener() {
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		
-		//GUIHome gui = new GUIHome(writer);
-	    
-		QWERTYKeyboard temp = new QWERTYKeyboard();
-		temp.text = package_width_text;
-		//QWERTYKeyboard.run();
-		Thread t = new Thread(temp);
-		t.start();
-		
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 
-});
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
-*/
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+/
